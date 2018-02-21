ff6432537e2b6fad6729ec9ad0150854989efbd8
weilbith <thore@weilbier.net>
2018-02-21 12:09:51 +0100

    First two segments with powerfont separator.
    Add new configurations for the height, position, the font and segment
    separators.
    For the font, a fork of the original lemonbar has been used, to use Xtf
    fonts.
    The font size is set in relation to the bar height by default.
    The bar display two example segments on the right with the separators.

--- i3-planetbar.sh
+++ i3-planetbar.sh
@@ -5,8 +5,16 @@ source $(dirname $0)/default.conf # Default values for all necessary variables.
 source $(dirname $0)/custom.conf # Load after default values to be able to overwrite them.
 
 
+# Define deviated variables.
+[[ "$BOTTOM" = true ]] && BAR_BOTTOM_ARG="-b"
+
+
 # Variables (load here to make sure they overwrite users configurations if necessary)
 FIFO="/tmp/i3_planetbar" # The fifo used to pass content to the lemonbar.
+
+
+# Load the fonts
+xset fp+ $HOME/.fonts/terminesspowerline
 
 
 # Prepare the fifo.
@@ -26,7 +34,13 @@ function writing {
     counter=$(($counter+1))
     
     # Format the content for the lemonbar and write it to the fifo.
-    printf "%s\n" "%{r}%{F#000000} ${counter} %{F- B-}" > "${FIFO}" &
+    firstRightBg="#ff0000"  
+    firstRightFg="#ffffff"
+    secondRightBg="#0000ff"
+    secondRightFg="#ffffff"
+    firstRightSeg="%{r}%{B${secondRightBg} F${firstRightBg}}${SEGMENT_SEPARATOR_RIGHT_OUTER}%{B${firstRightBg} F${firstRightFg}} T %{F- B-}"
+    secondRightSeg="%{r}%{B${DEFAULT_BACKGROUND} F${secondRightBg}}${SEGMENT_SEPARATOR_RIGHT_OUTER}%{F${secondRightFg} B${secondRightBg}} ${counter} %{F- B-}"
+    printf "%s\n" "${secondRightSeg}    ${firstRightSeg}" > "${FIFO}" &
    
     # Delay until update the content.
     sleep 1s
@@ -51,5 +65,5 @@ function reading {
 # Start the process chain with writing/reading the fifo and pass it to the powerline.
 writing & # Write content to the buffer in background.
 reading | # Read content from the buffer in background.
-lemonbar -p -g x30 -B "$DEFAULT_BACKGROUND" -F "$DEFAULT_FOREGROUND" & # Run lemonbar in background and read standard output from the reading function.
+$(dirname $0)/lemonbar -p -d "$BAR_BOTTOM_ARG" -f "$FONT_DEFAULT:size=$FONT_SIZE" -B "$DEFAULT_BACKGROUND" -F "$DEFAULT_FOREGROUND" -g "x$HEIGHT" & # Run lemonbar in background and read standard output from the reading function.
 wait # Wait here and do not end (useful for debugging, else just sent to background on call)
